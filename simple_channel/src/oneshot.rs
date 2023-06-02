38f4adb543454a2c041b18295c9a01d6354bc161
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-06-02 13:00:26 +0530

    oneshot implementation

--- simple_channel/src/oneshot.rs
+++ simple_channel/src/oneshot.rs
@@ -3,11 +3,41 @@
 // Unsafecell -> for storage
 // Atomic Bool -> state
 
-use std::sync::atomic::AtomicBool;
+use std::{ sync::atomic::AtomicBool, cell::UnsafeCell, mem::MaybeUninit };
+
+use std::sync::atomic::Ordering::{ Release, Acquire };
 
 pub struct Channel<T> {
-    queue: Option<T>,
+    message: UnsafeCell<MaybeUninit<T>>, //UnsafeCell<Option<T>>,
     ready: AtomicBool,
 }
 
-impl<T> Channel<T> {}
+// channel is safe to share if T is a send type
+
+unsafe impl<T> Sync for Channel<T> where T: Send {} //trait
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    /// Safety: Only call this once!
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release)
+    }
+
+    pub fn is_read(&self) -> bool {
+        self.ready.load(Acquire)
+    }
+
+    /// Safety: only call this once;
+    /// and only after is_read() returns True!
+    ///
+    pub unsafe fn receive(&self) -> T {
+        (*self.message.get()).assume_init_read()
+    }
+}
